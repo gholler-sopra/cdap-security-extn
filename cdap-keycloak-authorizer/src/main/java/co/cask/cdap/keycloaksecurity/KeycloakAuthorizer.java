9b6f8b8519185fe9ea615da4bc908bff2793a97a
sushil-singh-guavus <sushil.singh@guavus.com>
2020-01-06 14:14:51 +0530

    keycloak-authorization-extension

--- cdap-keycloak-authorizer/src/main/java/co/cask/cdap/keycloaksecurity/KeycloakAuthorizer.java
+++ cdap-keycloak-authorizer/src/main/java/co/cask/cdap/keycloaksecurity/KeycloakAuthorizer.java
@@ -1,19 +1,15 @@
 package co.cask.cdap.keycloaksecurity;
 
-import co.cask.cdap.security.spi.authorization.AbstractAuthorizer;
-
-import co.cask.cdap.proto.id.EntityId;
+import co.cask.cdap.proto.id.*;
 import co.cask.cdap.proto.security.*;
 import co.cask.cdap.security.spi.authorization.AbstractAuthorizer;
 import co.cask.cdap.security.spi.authorization.AuthorizationContext;
 import co.cask.cdap.security.spi.authorization.UnauthorizedException;
-import org.json.JSONObject;
-import org.json.XML;
-import org.keycloak.adapters.KeycloakDeployment;
-import org.keycloak.adapters.KeycloakDeploymentBuilder;
-import org.keycloak.adapters.rotation.AdapterTokenVerifier;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.keycloak.authorization.client.AuthorizationDeniedException;
 import org.keycloak.authorization.client.AuthzClient;
+import org.keycloak.authorization.client.Configuration;
+import org.keycloak.authorization.client.representation.TokenIntrospectionResponse;
 import org.keycloak.authorization.client.resource.ProtectedResource;
 import org.keycloak.representations.idm.authorization.AuthorizationRequest;
 import org.keycloak.representations.idm.authorization.AuthorizationResponse;
@@ -25,54 +21,71 @@ import org.slf4j.LoggerFactory;
 import java.io.*;
 import java.util.*;
 
-//import co.cask.cdap.security.server.KeycloakConfDeployment;
-
 public class KeycloakAuthorizer extends AbstractAuthorizer {
     private static final Logger LOG = LoggerFactory.getLogger(KeycloakAuthorizer.class);
+    private static AuthzClient authzClient;
+    private static Properties properties;
+    private String instanceName;
 
-    public KeycloakAuthorizer(){
+    public KeycloakAuthorizer() {
     }
 
     public void initialize(AuthorizationContext context) throws Exception {
         System.out.println("initializing ...");
+        properties = context.getExtensionProperties();
+        InputStream is = createConfigration(context);
+        instanceName = properties.containsKey("instance.name") ?
+                properties.getProperty("instance.name") : "cdap";
+        authzClient = AuthzClient.create(is);
     }
 
 
     @Override
     public void enforce(EntityId entity, Principal principal, Action action) throws Exception {
-        Set <Action> scopes = new HashSet();
+        Set<Action> scopes = new HashSet();
         scopes.add(action);
-        //Token is explicitly pass null as authorization changes not done in build
-        if(!enforce(entity.getEntityName(),scopes,null)){
+        if (!enforce(entity, scopes, principal.getAccessToken())) {
             throw new UnauthorizedException(principal, action, entity);
         }
     }
 
-    //    @Override
     public void enforce(EntityId entity, Principal principal, Set<Action> set) throws Exception {
         LOG.debug("Enforce called on entity {}, principal {}, actions {}", entity, principal, set);
         //TODO: Investigate if its possible to make the enforce call with set of actions rather than one by one
-        if(!enforce(entity.getEntityName(),set, null)){
+        if (!enforce(entity, set, principal.getAccessToken())) {
             throw new UnauthorizedException(principal, set, entity);
         }
     }
 
-    public boolean enforce(String resource, Set <Action> scopes , String accessToken) throws ClassNotFoundException{
-        List <String> scopeList = new ArrayList();
-        for(Action action : scopes){
+    public boolean enforce(EntityId entityId, Set<Action> scopes, String accessToken) {
+        List<String> scopeList = new ArrayList();
+        for (Action action : scopes) {
             scopeList.add(action.toString());
         }
-        boolean isAllowed = true;
-        isAllowed = requestTokenAuthorization(resource,scopeList,accessToken);
+        boolean isAllowed = isEntityAccessible(entityId, scopeList, accessToken);
+//        boolean isAllowed = requestTokenAuthorization(entityId.getEntityName(),scopeList,accessToken);
         return isAllowed;
     }
 
 
     public Set<? extends EntityId> isVisible(Set<? extends EntityId> entityIds, Principal principal) throws Exception {
-        //throw new UnsupportedOperationException("Visibility check is not supported via keycloak");
         Set<EntityId> visibleEntities = new HashSet(entityIds.size());
+        ArrayList<String> scopes = new ArrayList<String>(Arrays.asList("READ", "WRITE", "EXECUTE", "ADMIN"));
+        Map<String, List<String>> resourceMap = new HashMap();
+        Map<String, EntityId> entityMap = new HashMap();
         for (EntityId entityId : entityIds) {
-            visibleEntities.add(entityId);
+            resourceMap.put(entityId.getEntityName(), scopes);
+            entityMap.put(entityId.getEntityName(), entityId);
+        }
+        Collection<Permission> grantedPermission = requestTokenAuthorization(resourceMap, principal.getAccessToken());
+        if (grantedPermission.isEmpty())
+            return Collections.EMPTY_SET;
+        Iterator<Permission> permissionIterator = grantedPermission.iterator();
+        while (permissionIterator.hasNext()) {
+            String resourceName = permissionIterator.next().getResourceName();
+            if (entityMap.containsKey(resourceName)) {
+                visibleEntities.add(entityMap.get(resourceName));
+            }
         }
         return visibleEntities;
     }
@@ -119,95 +132,159 @@ public class KeycloakAuthorizer extends AbstractAuthorizer {
         throw new UnsupportedOperationException("Please use Ranger Admin UI to list privileges.");
     }
 
-    public  boolean requestTokenAuthorization(String resource , List<String> scopes, String keycloakToken) {
+
+    private boolean requestTokenAuthorization(String resource, List<String> scopes, String keycloakToken) {
 
         try {
+            if (keycloakToken == null) {
+                return false;
+            }
 
-            AuthorizationRequest authzRequest1 = new AuthorizationRequest();
-            if(authzRequest1!=null)
-                System.out.println("bsnsbcncnbc");
-
-            String path = Thread.currentThread().getContextClassLoader().getResource("cdap-site.xml").getPath();
-            InputStream is = createKeycloakDeployment(path);
-            KeycloakDeployment deployment = KeycloakDeploymentBuilder.build(is);
-
-
-            AuthzClient authzClient = AuthzClient.create(is);
             AuthorizationRequest authzRequest = new AuthorizationRequest();
-
             AuthorizationResponse authzResponse;
-
             ProtectedResource resourceClient = authzClient.protection().resource();
-            ResourceRepresentation existingResource = resourceClient.findByName(resource);
-            authzRequest.addPermission(existingResource.getId(),scopes);
-
+            ResourceRepresentation existingResource = resourceClient.findByMatchingUri(resource).get(0);
+            authzRequest.addPermission(existingResource.getId(), scopes);
             authzResponse = authzClient.authorization(keycloakToken).authorize(authzRequest);
-
             if (authzResponse != null) {
-              org.keycloak.representations.AccessToken rptToken =  AdapterTokenVerifier.verifyToken(authzResponse.getToken(), deployment);
-              org.keycloak.representations.AccessToken.Authorization authorization = rptToken.getAuthorization();
-
-                if (authorization != null) {
-                    Collection<Permission> grantedPermissions= authorization.getPermissions();
+                TokenIntrospectionResponse requestingPartyToken = authzClient.protection().introspectRequestingPartyToken(authzResponse.getToken());
+                if (requestingPartyToken != null) {
+                    Collection<Permission> grantedPermissions = requestingPartyToken.getPermissions();
                     Permission resourcePermission = grantedPermissions.iterator().next();
-                    if(resourcePermission.getResourceId() == resource && resourcePermission.getScopes().containsAll(scopes)){
+                    if (resourcePermission.getResourceId().equals(existingResource.getId()) && resourcePermission.getScopes().containsAll(scopes)) {
                         return true;
                     }
                 }
             }
         } catch (AuthorizationDeniedException ignore) {
-            //LOGGER.debug("Authorization denied", ignore);
             throw new RuntimeException("Unexpected error during authorization request.", ignore);
 
         } catch (Exception e) {
             throw new RuntimeException("Unexpected error during authorization request.", e);
         }
-        return true;
+        return false;
     }
 
 
-    public static InputStream createKeycloakDeployment(String Configfile){
+    private Collection requestTokenAuthorization(Map<String, List<String>> resourceMap, String keycloakToken) {
 
         try {
-            File xmlFile = new File(Configfile);
-            Reader fileReader = new FileReader(xmlFile);
-            BufferedReader bufReader = new BufferedReader(fileReader);
-            boolean flag = false;
-            StringBuilder sb = new StringBuilder();
-            String line = bufReader.readLine().trim();
-            while (line != null) {
-                if (line.endsWith("</keycloakConfiguration>")) {
-                    flag = false;
-                    break;
-                }
-                if (line.endsWith("<keycloakConfiguration>") || flag == true) {
-                    if(flag)
-                        sb.append(line).append("\n");
-                    flag=true;
-                }
-                line = bufReader.readLine().trim();
+            if (keycloakToken == null) {
+                throw new RuntimeException("token is not available");
             }
 
-            if(sb.length()!=0) {
-                String xml2String = sb.toString();
+            if (resourceMap.isEmpty())
+                return Collections.EMPTY_LIST;
 
-                try {
-                    JSONObject obj = XML.toJSONObject(xml2String);
-                    String str = obj.toString();
-                    InputStream is = new ByteArrayInputStream(str.getBytes());
-                    return is;
-                }
-                catch (Exception ex){
-                    throw new RuntimeException("error in converting xml to json");
-                }
-            }
-            else{
-                throw new RuntimeException("Keycloak configuration is not defined");
+            AuthorizationRequest authzRequest = new AuthorizationRequest();
+            AuthorizationResponse authzResponse;
+            ProtectedResource resourceClient = authzClient.protection().resource();
+
+            for (Map.Entry<String, List<String>> resource : resourceMap.entrySet()) {
+                ResourceRepresentation existingResource = resourceClient.findByName(resource.getKey());
+                if (existingResource != null)
+                    authzRequest.addPermission(existingResource.getId(), resource.getValue());
             }
 
+            authzResponse = authzClient.authorization(keycloakToken).authorize(authzRequest);
+            TokenIntrospectionResponse requestingPartyToken = authzClient.protection().introspectRequestingPartyToken(authzResponse.getToken());
+            if (requestingPartyToken != null) {
+                Collection<Permission> grantedPermissions = requestingPartyToken.getPermissions();
+                return grantedPermissions;
+            }
+        } catch (AuthorizationDeniedException ignore) {
+            throw new RuntimeException("Unexpected error during authorization request.", ignore);
+
+        } catch (Exception e) {
+            throw new RuntimeException("Unexpected error during authorization request.", e);
         }
-        catch(Exception ex){
-            throw new RuntimeException(ex.getMessage());
+        return Collections.EMPTY_LIST;
+    }
+
+
+    public boolean isEntityAccessible(EntityId entityId, List<String> scopes, String accessToken) {
+        String entityType = entityId.getEntityType().name();
+        Map<String, Integer> entityMap = new HashMap<String, Integer>();
+        entityMap.put("INSTANCE", 1);
+        entityMap.put("NAMESPACE", 2);
+        entityMap.put("ARTIFACT", 3);
+        entityMap.put("APPLICATION", 4);
+        entityMap.put("DATASET", 5);
+        entityMap.put("DATASET_MODULE", 6);
+        entityMap.put("DATASET_TYPE", 7);
+        entityMap.put("PROGRAM", 8);
+        entityMap.put("SECUREKEY", 9);
+        entityMap.put("KERBEROSPRINCIPAL", 10);
+        String resourceUrl;
+        switch (entityMap.get(entityType)) {
+            case 1:
+                resourceUrl = "instance/";
+                break;
+            case 2:
+                resourceUrl = "instance/" + instanceName + "/namespace/";
+                break;
+            case 3:
+                ArtifactId artifactId = (ArtifactId) entityId;
+                resourceUrl = "instance/" + instanceName + "/namespace/" + artifactId.getNamespace() + "/artifact/" + artifactId.getArtifact();
+                break;
+            case 4:
+                ApplicationId applicationId = (ApplicationId) entityId;
+                resourceUrl = "instance/" + instanceName + "/namespace/" + applicationId.getNamespace() + "/application/" + applicationId.getApplication();
+                break;
+            case 5:
+                DatasetId dataset = (DatasetId) entityId;
+                resourceUrl = "instance/" + instanceName + "/namespace/" + dataset.getNamespace() + "/application/" + dataset.getDataset();
+                break;
+            case 6:
+                DatasetModuleId datasetModuleId = (DatasetModuleId) entityId;
+                resourceUrl = "instance/" + instanceName + "/namespace/" + datasetModuleId.getNamespace() + "/datasetmodule/" + datasetModuleId.getModule();
+                break;
+            case 7:
+                DatasetTypeId datasetTypeId = (DatasetTypeId) entityId;
+                resourceUrl = "instance/" + instanceName + "/namespace/" + datasetTypeId.getNamespace() + "/datasettype/" + datasetTypeId.getType();
+                break;
+            case 8:
+                ProgramId programId = (ProgramId) entityId;
+                resourceUrl = "instance/" + instanceName + "/namespace/" + programId.getNamespace() + "/application/" + programId.getApplication() + "/program/" + programId.getProgram();
+                break;
+            case 9:
+                SecureKeyId secureKeyId = (SecureKeyId) entityId;
+                resourceUrl = "instance/" + instanceName + "/namespace/" + secureKeyId.getNamespace() + "/securekey/" + secureKeyId.getName();
+                break;
+            case 10:
+                KerberosPrincipalId kerberosPrincipalId = (KerberosPrincipalId) entityId;
+                resourceUrl = "instance/" + instanceName + "/kerberosprincipal/" + kerberosPrincipalId.getPrincipal();
+                break;
+            default:
+                throw new IllegalArgumentException(String.format("The entity %s is of unknown type %s", entityId, entityType));
         }
+
+        if (!resourceUrl.equals("") && !scopes.equals("")) {
+            return requestTokenAuthorization(resourceUrl, scopes, accessToken);
+        }
+        return false;
+    }
+
+    private InputStream createConfigration(AuthorizationContext context) {
+        Properties extensionProp = context.getExtensionProperties();
+        String JsonElem;
+        String clientId = extensionProp.getProperty("client_id");
+        String clientSecret = extensionProp.getProperty("client_secret");
+        String realm = extensionProp.getProperty("realm");
+        String authServerUrl = extensionProp.getProperty("authserverurl");
+
+        Map<String, Object> clientCredentials = new HashMap();
+        clientCredentials.put("secret", clientSecret);
+        Configuration keycloakConf = new Configuration(authServerUrl, realm, clientId, clientCredentials, null);
+        ObjectMapper objectMapper = new ObjectMapper();
+        try {
+            JsonElem = objectMapper.writeValueAsString(keycloakConf);
+            System.out.println(JsonElem);
+        } catch (Exception ex) {
+            throw new RuntimeException("unable to convert to Json");
+        }
+
+        InputStream inputStream = new ByteArrayInputStream(JsonElem.getBytes());
+        return inputStream;
     }
 }
